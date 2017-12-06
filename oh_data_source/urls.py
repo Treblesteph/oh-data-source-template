ea01b22f0eaecdd60457f23c053e25866547b868
Steph Smith-Unna <steph.unna@gmail.com>
2017-12-06 14:23:31 +0300

    Adding names to urls to refer easily.

--- oh_data_source/urls.py
+++ oh_data_source/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
-    url(r'complete/?$', views.complete),
+    url(r'^$', views.index, name='index'),
+    url(r'complete/?$', views.complete, name='complete'),
 ]
