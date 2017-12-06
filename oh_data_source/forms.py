bb8551a5b63af7595eb511269f52f3f7bb1c6b2f
Steph Smith-Unna <steph.unna@gmail.com>
2017-12-06 19:23:18 +0300

    Adding in label and help text to form.

--- oh_data_source/forms.py
+++ oh_data_source/forms.py
@@ -2,5 +2,9 @@ from django import forms
 
 
 class UploadFileForm(forms.Form):
+    print('making uploader')
     title = forms.CharField(max_length=50)
-    file = forms.FileField(label='Select twitter archive file')
+    file = forms.FileField(
+        label='Select file',
+        help_text='please select your twitter archive'
+        )
