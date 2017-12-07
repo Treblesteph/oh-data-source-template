58d14ec6080cfbb440fefba096c24e87faccf677
Steph Smith-Unna <steph.unna@gmail.com>
2017-12-07 17:33:30 +0300

    Removing title.

--- oh_data_source/forms.py
+++ oh_data_source/forms.py
@@ -3,7 +3,6 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     print('making uploader')
-    title = forms.CharField(max_length=50)
     file = forms.FileField(
         label='Select file',
         help_text='please select your twitter archive'
